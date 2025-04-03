5c5771f259fb68f6c9a8e8e7e2bb109af2a0237b
Jack Sellars <jsellars@enee.com.au>
2025-04-04 09:50:09 +1030

    fix: error checking

--- src/leafnode.py
+++ src/leafnode.py
@@ -6,4 +6,8 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
