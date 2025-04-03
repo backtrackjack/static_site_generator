aceaa4ef1273fcc41029037475bb76007d3b5ac0
Jack Sellars <jsellars@enee.com.au>
2025-04-04 09:34:15 +1030

    test: add tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,12 +3,24 @@ from htmlnode import HTMLNode
 
 
 class TestTextNode(unittest.TestCase):
-    def test_props_to_html(self):
+    def test_props_to_html_simple(self):
+        node = HTMLNode("text", props={"class": "text", "id": "1234"})
+        self.assertEqual(node.props_to_html(), ' class="text" id="1234"')
+
+    def test_props_to_html_empty(self):
+        node = HTMLNode("text", props={})
+        self.assertEqual(node.props_to_html(), "")
+
+    def test_props_to_html_complex(self):
         node = HTMLNode(
             "text",
-            props={"class": "text", "id": "1234"}
+            props={
+                "class": "text",
+                "id": "1234",
+                "style": "color: red; font-size: 12px;",
+            },
         )
         self.assertEqual(
             node.props_to_html(),
-            ' class="text" id="1234"'
+            ' class="text" id="1234" style="color: red; font-size: 12px;"',
         )
