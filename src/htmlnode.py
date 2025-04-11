692810e3962978f31403dd9904f361a38fad3b20
Jack Sellars <jsellars@enee.com.au>
2025-04-11 12:39:08 +0930

    getting up to speed

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,6 +1,3 @@
-from functools import reduce
-
-
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
@@ -9,25 +6,18 @@ class HTMLNode:
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError()
+        raise NotImplementedError("to_html method not implemented")
 
     def props_to_html(self):
         if self.props is None:
             return ""
-        return reduce(
-            lambda x,
-            k: x + f' {k}="{self.props[k]}"',
-            self.props,
-            ""
-        )
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
 
     def __repr__(self):
-        return f"HTMLNode( \
-            tag={self.tag}, \
-            value={self.value}, \
-            children={self.children}, \
-            props={self.props} \
-        )"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
 
 
 class LeafNode(HTMLNode):
@@ -41,6 +31,9 @@ class LeafNode(HTMLNode):
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
 
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
@@ -49,6 +42,12 @@ class ParentNode(HTMLNode):
     def to_html(self):
         if self.tag is None:
             raise ValueError("invalid HTML: no tag")
-        return f"<{self.tag}{self.props_to_html()}>" + \
-            "".join([child.to_html() for child in self.children]) + \
-            f"</{self.tag}>"
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,8 @@
 import unittest
-from functions import (
+from inline_markdown import (
     split_nodes_delimiter,
+    extract_markdown_links,
+    extract_markdown_images,
 )
 
 from textnode import TextNode, TextType
@@ -8,27 +10,27 @@ from textnode import TextNode, TextType
 
 class TestInlineMarkdown(unittest.TestCase):
     def test_delim_bold(self):
-        node = TextNode("This is text with a **bolded** word", TextType.NORMAL)
+        node = TextNode("This is text with a **bolded** word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
         self.assertListEqual(
             [
-                TextNode("This is text with a ", TextType.NORMAL),
+                TextNode("This is text with a ", TextType.TEXT),
                 TextNode("bolded", TextType.BOLD),
-                TextNode(" word", TextType.NORMAL),
+                TextNode(" word", TextType.TEXT),
             ],
             new_nodes,
         )
 
     def test_delim_bold_double(self):
         node = TextNode(
-            "This is text with a **bolded** word and **another**", TextType.NORMAL
+            "This is text with a **bolded** word and **another**", TextType.TEXT
         )
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
         self.assertListEqual(
             [
-                TextNode("This is text with a ", TextType.NORMAL),
+                TextNode("This is text with a ", TextType.TEXT),
                 TextNode("bolded", TextType.BOLD),
-                TextNode(" word and ", TextType.NORMAL),
+                TextNode(" word and ", TextType.TEXT),
                 TextNode("another", TextType.BOLD),
             ],
             new_nodes,
@@ -36,53 +38,74 @@ class TestInlineMarkdown(unittest.TestCase):
 
     def test_delim_bold_multiword(self):
         node = TextNode(
-            "This is text with a **bolded word** and **another**", TextType.NORMAL
+            "This is text with a **bolded word** and **another**", TextType.TEXT
         )
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
         self.assertListEqual(
             [
-                TextNode("This is text with a ", TextType.NORMAL),
+                TextNode("This is text with a ", TextType.TEXT),
                 TextNode("bolded word", TextType.BOLD),
-                TextNode(" and ", TextType.NORMAL),
+                TextNode(" and ", TextType.TEXT),
                 TextNode("another", TextType.BOLD),
             ],
             new_nodes,
         )
 
     def test_delim_italic(self):
-        node = TextNode("This is text with an _italic_ word", TextType.NORMAL)
+        node = TextNode("This is text with an _italic_ word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
         self.assertListEqual(
             [
-                TextNode("This is text with an ", TextType.NORMAL),
+                TextNode("This is text with an ", TextType.TEXT),
                 TextNode("italic", TextType.ITALIC),
-                TextNode(" word", TextType.NORMAL),
+                TextNode(" word", TextType.TEXT),
             ],
             new_nodes,
         )
 
     def test_delim_bold_and_italic(self):
-        node = TextNode("**bold** and _italic_", TextType.NORMAL)
+        node = TextNode("**bold** and _italic_", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
         new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
-        self.assertListEqual(
+        self.assertEqual(
             [
                 TextNode("bold", TextType.BOLD),
-                TextNode(" and ", TextType.NORMAL),
+                TextNode(" and ", TextType.TEXT),
                 TextNode("italic", TextType.ITALIC),
             ],
             new_nodes,
         )
 
     def test_delim_code(self):
-        node = TextNode("This is text with a `code block` word", TextType.NORMAL)
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
         self.assertListEqual(
             [
-                TextNode("This is text with a ", TextType.NORMAL),
+                TextNode("This is text with a ", TextType.TEXT),
                 TextNode("code block", TextType.CODE),
-                TextNode(" word", TextType.NORMAL),
+                TextNode(" word", TextType.TEXT),
             ],
             new_nodes,
         )
 
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://blog.boot.dev"),
+            ],
+            matches,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
