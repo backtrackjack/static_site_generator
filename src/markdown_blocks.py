6218afe7ba6e912814b6f0789b02de72e656a9a4
Jack Sellars <jsellars@enee.com.au>
2025-04-11 13:28:37 +0930

    block to block type

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,6 +1,32 @@
+from enum import Enum
+
+
 def markdown_to_blocks(markdown):
     return [
         "\n".join(line.strip() for line in block.strip().split("\n"))
         for block in markdown.split("\n\n")
         if block.strip()
     ]
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+
+def block_to_block_type(block):
+    if block.startswith("#"):
+        return BlockType.HEADING
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        return BlockType.ULIST
+    if block[0].isdigit() and block[1:3] == ". ":
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
